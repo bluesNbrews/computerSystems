b74676abf9f39e5e9f15e534005b6a55008bdf82
Steven Williams <mstevenwilliams@gmail.com>
2019-02-19 22:47:24 -0500

    Create C program to find sum of array elements using Dynamic Memory Allocation. First commit includes getting array size from the user.

---
+++ dynamicMemAllocation.c
@@ -0,0 +1,17 @@
+//Steven Williams
+//CSCI 6011 
+//C program to find sum of array elements using Dynamic Memory Allocation.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+	int arraySize;
+	
+	printf("Please provide the size of the array: ");
+	scanf("%d", &arraySize);
+	printf("%d\n", arraySize);
+	
+	return 0;
+}
