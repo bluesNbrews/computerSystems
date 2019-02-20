665e37929c24b34d09305bf8a9ffa80ed0b1a08c
Steven Williams <mstevenwilliams@gmail.com>
2019-02-20 16:15:29 -0500

    Allocate memory. Get user input for array. Print the array. Get sum of array elemets and print. Also free memory at the end

--- dynamicMemAllocation.c
+++ dynamicMemAllocation.c
@@ -5,13 +5,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){
+int main()
+{	
+	int arraySize;	
+	int *ptr;
+	int sum = 0;	
 
-	int arraySize;
-	
-	printf("Please provide the size of the array: ");
+	//Get user input for array size
+	printf("What is the size of the array: ");
 	scanf("%d", &arraySize);
-	printf("%d\n", arraySize);
+
+	//Dynamically allocate memory for the array
+	ptr = (int*)malloc(arraySize * sizeof(int));
+
+	//Get user input for the array 
+	for(int i = 0; i < arraySize; i++)
+	{
+		printf("What is element %02d: ", i + 1);
+		scanf("%d", (ptr + i));
+	}
+
+	//Print the array
+	printf("The array is as follows:\n");
+	for(int i = 0; i < arraySize; i++)
+	{
+		printf("%d\n", *(ptr + i));
+	}
+	
+	//Add all of the array elements
+	for(int i = 0; i < arraySize; i++)
+	{
+		sum += *(ptr+i);
+	}
+	printf("The sum of all of the array elements is: %d\n", sum);
+	
+	//Free the memory!
+	free(ptr);	
 	
 	return 0;
 }
