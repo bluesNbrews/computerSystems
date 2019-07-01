1e80c2a6d22a785f58f607a035a6896f3b5b3055
Steven <mstevenwilliams@gmail.com>
2019-07-01 14:30:30 -0400

    Small change to block size calculation logic.

--- CSCI6461/src/add.cu
+++ CSCI6461/src/add.cu
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <math.h>
-// Kernel function to add the elements of two arrays
 
+// Kernel function to add the elements of two arrays
 __global__
 void add(int n, float *x, float *y)
 {
-  int index = threadIdx.x;
-  int stride = blockDim.x;
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
   for (int i = index; i < n; i += stride)
-      y[i] = x[i] + y[i];
+    y[i] = x[i] + y[i];
 }
 
 int main(void)
@@ -27,7 +27,9 @@ int main(void)
   }
 
   // Run kernel on 1M elements on the GPU
-  add<<<1, 256>>>(N, x, y);
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  add<<<numBlocks, blockSize>>>(N, x, y);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
