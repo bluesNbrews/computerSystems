1094db08eb24c3923ecf48e6d59ec4039c71dd11
Steven <mstevenwilliams@gmail.com>
2019-09-24 11:01:20 -0400

    Clean up comments and rebuild.

--- CSCI6011/src/binomialCoeff.c
+++ CSCI6011/src/binomialCoeff.c
@@ -1,17 +1,19 @@
-//Author: Steven Williams
-//CSCI 6011
-//Implement the binomial coeffecient formula in C (for nonnegative integers)
+//  Author: Steven Williams
+//  Implement the binomial coeffecient formula in C (for nonnegative integers)
 /*
 	|n|	      n!    
 	| |	= ----------
 	|k|    k!(n-k)!
 */
+
 #include<stdio.h>
 
 //Method to calculate a factorial
 int factorial(int f){
+	
 	//Set i to one less than parameter to decrement down to 0
 	int i = f - 1;
+	
 	//Give sum the value of the number for factorial. It will then be multiplied
 	//by the i abovee
 	int sum = f;
@@ -19,27 +21,32 @@ int factorial(int f){
 		sum *= i;
 		i--;
 	}
+	
 	//printf("%d\n", sum);
 	return sum;
 }
 
 int formula(int n, int k){
+	
 	int result = 0;
+	
 	//See formula above. Use factorial function to calculate
 	result = factorial(n) / (factorial(k) * (factorial(n-k)));
+	
 	//Return factorial result of parameter
 	return result;
 }
 
 int main(){
+	
 	int total = 0;
 	int n = 0;
 	int k = 0;
 
 	//Use input for n and k of the formula
-	printf("Please enter 'n' for the formula: \n");
+	printf("Please enter 'n' for the formula (e.g. 7): \n");
 	scanf("%d", &n);
-	printf("Please enter 'k' for the formula: \n");
+	printf("Please enter 'k' for the formula (e.g. 2): \n");
 	scanf("%d", &k);
 	
 	//Calculate formula with the user variables above
