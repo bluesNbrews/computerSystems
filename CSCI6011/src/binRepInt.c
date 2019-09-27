01dc938062b366d015cd59943dee9e2d4db73a48
Steven <mstevenwilliams@gmail.com>
2019-09-27 13:02:51 -0400

    Cleanup comments.

--- CSCI6011/src/binRepInt.c
+++ CSCI6011/src/binRepInt.c
@@ -1,6 +1,5 @@
-//Author: Steven Williams
-//CSCI 6011
-
+// Author: Steven Williams
+// CSCI 6011
 /*
 Write a C program to count number of 1's in the binary representation of an integer.
 Loop through all bits in an integer, find if a bit is set and if it has incremented
@@ -17,12 +16,11 @@ Algorithm Steps:
 
 int intToBinary(int userInt){
 
-	//The temporary number is essentially a remainder to iterate through the binary version of the integer. See below.
+	//The temporary number is essentially a remainder to iterate through the binary version of the integer (1 byte, 0-255). See below.
 	//The total binary 1's variable is a counter for the 1's .
 	//OK, so the test array is used to subtract the user integer from each array number (8 numbers total, one for each bit. For ex: 2^7 = 128, 2^6 = 64, ..., 2^0 = 1).   
 	//If the integer is greater than the ith number, it can be subtracted and the counter can be incremented to show that a 1 exists in that position of the byte.  
 	//The remainder will be used as the loop iterates through the bit values for each position in binary.
-
 	int tempNum = userInt;
 	int totalBin = 0;
 	int testArray[] = {
@@ -42,12 +40,13 @@ int intToBinary(int userInt){
 }
 
 int main(){
+	
 	//Variables for user input and for the total number of 1's in the binary form of the integer, respectfully.
 	int userInt = 0;
 	int totalOnes = 0;
 	
 	//Take user input for an integer
-	printf("%s\n", "Please enter an integer.");
+	printf("%s\n", "Please enter an integer (0-255).");
 	scanf("%d", &userInt);
 
 	//Call the function to count the number of 1's and store in the respective variable.
