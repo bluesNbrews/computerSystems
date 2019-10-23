df9e86af40017ed901437280a4ba74f0233db53d
Steven <mstevenwilliams@gmail.com>
2019-10-23 10:55:12 -0400

    Cleanup comments and rebuild.

--- CSCI6011/src/trailZeroBin.c
+++ CSCI6011/src/trailZeroBin.c
@@ -1,5 +1,4 @@
 //Author: Steven Williams
-//CSCI 6011
 
 /*Write source code to enter any number by the user to count the number of trailing zeros in the given number by using the 
 bitwise operator. A bitwise operator is used to track the trailing zeros in any given number. The first set bit position is 
@@ -8,16 +7,14 @@ equal to the number of the trailing zeros in the binary number.*/
 #include <stdio.h>
 
 int main(){
-	//
+	
 	int num, i;
 	int count = 0;
 	int sizeInt = sizeof(int) * 8;
-	//printf("%d", sizeInt);
 
 	//Get number from user
 	printf("Enter any number: ");
 	scanf("%d", &num);
-	//printf("%d", num);
 
 	//Iterate over each bit of the number 
 	for(i = 0; i < sizeInt; i++){
@@ -26,8 +23,6 @@ int main(){
 		//form of the number to reach the 2^0 position (least sig bit position in little endian).
 		//printf("The iteration is currently at :%d\n", i);
 		if((num >> i ) & 1){
-			//printf("%d\n", num >> i);
-			//printf("%d\n\n", (num >> i) & 1);
 			break;
 		}
 		count++;
