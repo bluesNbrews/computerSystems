51411404ba83bab1105721f3f69ca8e76aab79f6
Steven <mstevenwilliams@gmail.com>
2019-11-15 18:07:24 -0500

    Finish queue header file.

--- CSCI6212/Queue.h
+++ CSCI6212/Queue.h
@@ -1,6 +1,7 @@
 #indef Queue_h
 #define Queue_h
 #include <stdlib.h>
+//Header file for queue data structure. Provides enqueue and dequeue functions for a queue.
 
 struct Node{
 	int data;
@@ -8,10 +9,37 @@ struct Node{
 } *front = NULL, *rear = NULL;
 
 void enqueue(int x){
+	struct Node *t;
+	t = (struct Node*)malloc(sizeof(struct Node));
+	
+	if(t == NULL)
+		printf("Queue is FULL\n");
+	else{
+		t -> data = x;
+		t -> next = NULL;
 
+		if(front == NULL)
+			front = rear = t;
+		else{
+			rear -> next = t;
+			rear = t;
+		}
+	}
 }
 
 void dequeue(){
+	int x = -1;
+	struct Node* t;
+
+	if(front == NULL)
+		printf("Queue is Empty\n");
+	else{
+		x = front -> data;
+		t = front;
+		front = front -> next;
+		free(t);
+	}
+	return x;
 	
 }
 
